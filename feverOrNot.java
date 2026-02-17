a5b922dad302a52afb95dbc412ad5941286a6c95
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-02-17 23:16:11 +0530

    Added new Java practice questions

---
+++ feverOrNot.java
@@ -0,0 +1,18 @@
+package DSA_JAVA;
+
+import java.util.Scanner;
+
+//You have a fever if your temperaure is above 100 and otherwise prints you don't have a fever
+
+public class feverOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Temperature");
+        double temp = sc.nextDouble();
+        if (temp >100){
+            System.out.println("You have a Fever");
+        }else{
+            System.out.println("You don't have a Fever");
+        }
+    }
+}
---
+++ largestOf3.java
@@ -0,0 +1,26 @@
+package DSA_JAVA;
+
+// Print the largest of 3
+
+import java.util.Scanner;
+
+public class largestOf3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of 'A': " );
+        int a = sc.nextInt();
+        System.out.println("Enter the value of 'B': ");
+        int b = sc.nextInt();
+        System.out.println("Enter the value of 'C': ");
+        int c = sc.nextInt();
+
+        if (a > b && a >= 0) {
+            System.out.println("A is greatest" + a);
+        } else if (b > -a && b >= c) {
+            System.out.println("B is greatest" + b);
+        } else {
+            System.out.println("C is greatest" + c);
+        }
+
+    }
+}
