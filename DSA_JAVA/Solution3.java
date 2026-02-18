dee4f028a2dd7d1367c2e27baf4ded7979cec36b
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-02-18 17:47:05 +0530

    added new problems

---
+++ DSA_JAVA/Solution3.java
@@ -0,0 +1,23 @@
+package DSA_JAVA;
+
+// Enter cost of 3 items from the user (using float data types) a pencil, a pen, and a earaser. You have to output the total cost of the items back to the user as their bill.
+import java.util.*;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the cost of a Pencil: ");
+        float pencil = sc.nextFloat();
+        System.out.println("Enter the cost of a Pen: ");    
+        float pen = sc.nextFloat();
+        System.out.println("Enter the cost of a Eraser: ");
+        float eraser = sc.nextFloat();
+        float totalCost = pencil + pen + eraser;
+        System.out.println("The Total Cost of the items is: " + totalCost);
+
+        // Add on-with  18% tax
+
+        float newTool = totalCost + (0.18f * totalCost);
+        System.out.println("Bill with 18% tax: " +newTool);
+    }
+}
---
+++ DSA_JAVA/eligibleForVote.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//Eligible for voting or not
+
+public class eligibleForVote {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Age: ");
+        int age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Eligible for Voting");
+        }
+        if (age > 13 && age < 18) {
+            System.out.println("Teenager Not yet Eligible for Voting ");
+        } else {
+            System.out.println("Not Eligible for voting");
+        }
+
+    }
+}
