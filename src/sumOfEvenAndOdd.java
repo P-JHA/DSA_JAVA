a2ec9ede1b1959379da0d2a3625ebfbff877aeab
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-02-21 22:15:00 +0530

    added problem SumOfEvenAndOddInteger

---
+++ src/sumOfEvenAndOdd.java
@@ -0,0 +1,28 @@
+package src;
+
+import java.util.Scanner;
+
+public class sumOfEvenAndOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number; 
+        int choice;
+        int evensum = 0;
+        int oddsum = 0;
+
+        do{
+            System.out.println("Enter the Number:");
+            number = sc.nextInt();
+
+            if(number % 2 == 0){
+                evensum += number;
+            }else{
+                oddsum += number;
+            }
+            System.out.println("Do you want to continue ? Press 1 for yes or 0  for no");
+            choice = sc.nextInt();
+        }while(choice == 1);
+        System.out.println("Sum of even numbers: " + evensum);
+        System.out.println("Sum of odd numbers" + oddsum);
+    }
+}
