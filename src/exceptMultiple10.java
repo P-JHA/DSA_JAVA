73c5c04dc314699554835795f31d3a39662dda80
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-02-25 21:38:41 +0530

    added new problem upon continue statement

---
+++ src/exceptMultiple10.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.util.Scanner;
+
+public class exceptMultiple10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        do{
+            System.out.println("Enter your number: ");
+            int n = sc.nextInt();
+            if(n % 10 == 0){
+                continue;
+            }
+            System.out.println("Number was : "+n);
+        }while(true);
+    }
+}
