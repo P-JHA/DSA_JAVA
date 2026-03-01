2520a898f070e26c1867cce5a1477b5548fe1cae
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-03-01 22:01:35 +0530

    Print all Primes in a Range

---
+++ src/primesInARange.java
@@ -0,0 +1,32 @@
+package src;
+
+public class primesInARange {
+
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void primesInARange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        primesInARange(100);
+    }
+}
