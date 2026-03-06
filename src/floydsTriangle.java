842c3b5cca68c318c6a4425457fb0b285d7e5243
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-03-06 22:19:59 +0530

    Floyd's Triangle

---
+++ src/floydsTriangle.java
@@ -0,0 +1,19 @@
+package src;
+
+public class floydsTriangle {
+    public static void triangle(int n){
+        int counter =1;
+        //outer
+        for(int i=1;i<=n;i++){
+            //inner - how many times will counter be printed
+            for(int j=1; j<=i; j++){
+                System.out.print(counter +" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        floydsTriangle.triangle(5);
+    }
+}
