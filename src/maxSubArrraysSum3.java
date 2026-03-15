6f7729d3321ca23a1d12539397aa109f8752f384
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-03-15 22:39:08 +0530

    Max SubArray Sum : 3 Using Kadane's Algorithm

---
+++ src/maxSubArrraysSum3.java
@@ -0,0 +1,21 @@
+package src;
+
+public class maxSubArrraysSum3 {
+    public static void kadanes(int numbers[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            cs = cs + numbers[i];
+            if (cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("Our Max Subarray Sum Is : "+ ms);
+    }
+    public static void main(String[] args) {
+        int numbers[] = {-2,-3,-4,-1,-2,1,5,-3};
+        kadanes(numbers);
+    }
+}
