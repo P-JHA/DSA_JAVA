541f3cdd2e6891ecdad8e728a5a0215e5e9bf25a
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-03-19 22:37:46 +0530

    Added New Problem

---
+++ src/Matrices.java
@@ -0,0 +1,48 @@
+package src;
+
+import java.util.Scanner;
+
+public class Matrices {
+
+    public static boolean search(int matrix[][], int key) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("Found at the Cell (" + i + "," + j + ")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Key Not Found");
+        return false;
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = new int[3][3];
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // output
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        search(matrix, 5);
+        sc.close();
+    }
+}
