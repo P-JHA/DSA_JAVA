0189a3ce9327c084cee7adf507e287c1d9898cf2
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-03-23 22:32:08 +0530

    Palindrome Or Not

---
+++ src/palindromeOrNot.java
@@ -0,0 +1,20 @@
+package src;
+
+public class palindromeOrNot {
+    public static boolean isPalindrome(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                //not palindrome
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "racecar";
+        //String str = "noon";
+        //String str = "madam";
+        System.out.println(isPalindrome(str));
+    }
+}
