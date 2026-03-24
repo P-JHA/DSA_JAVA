4be40f455192396d94636ab7dd646b3011d1db8e
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-03-24 22:24:23 +0530

    Shortest Path To Reach The Destination

---
+++ src/shortestPath.java
@@ -0,0 +1,38 @@
+package src;
+
+public class shortestPath {
+    public static float getShortedPath(String path){
+        int x = 0, y = 0;
+
+        for(int i = 0; i < path.length(); i++){
+            char dir = path.charAt(i);
+
+            // south
+            if(dir == 'S'){
+                y--;
+            }
+            // north
+            else if(dir == 'N'){
+                y++;
+            }
+            // west
+            else if(dir == 'W'){
+                x--;
+            }
+            // east
+            else if(dir == 'E'){
+                x++;
+            }
+        }
+
+        int x2 = x * x;
+        int y2 = y * y;
+
+        return (float) Math.sqrt(x2 + y2);
+    }
+
+    public static void main(String[] args) {
+        String path = "WNEENESENN";
+        System.out.println(getShortedPath(path));
+    }
+}
