520775e91ffe50d10fb92fb8ffb7b54a33dee5b3
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-03-26 22:06:08 +0530

    Added New Problem

--- src/firstLetterUpperCase.java
+++ src/firstLetterUpperCase.java
@@ -1,23 +1,26 @@
 package src;
 
 public class firstLetterUpperCase {
-    public static String StringBuilder(String str){
-        StringBuilder sb = new StringBuilder(" ");
 
+    public static String toUpperCase(String str){
+        StringBuilder sb = new StringBuilder();
+
+        // First character
         char ch = Character.toUpperCase(str.charAt(0));
         sb.append(ch);
 
-        for(int i=1; i<str.length(); i++){
-            if(str.charAt(i) == ' ' && i<str.length()-1){
-                sb.append(str.charAt(i));
+        for(int i = 1; i < str.length(); i++){
+            if(str.charAt(i) == ' ' && i < str.length() - 1){
+                sb.append(str.charAt(i));  // space
                 i++;
                 sb.append(Character.toUpperCase(str.charAt(i)));
-            }else{
+            } else {
                 sb.append(str.charAt(i));
             }
         }
         return sb.toString();
     }
+
     public static void main(String[] args) {
         String str = "hi, i am prabhakar";
         System.out.println(toUpperCase(str));
