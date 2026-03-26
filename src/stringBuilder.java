2181cfefaf938f57f0a1ffffd47d066bff0a629c
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-03-26 21:53:06 +0530

    String Builder

---
+++ src/stringBuilder.java
@@ -0,0 +1,11 @@
+package src;
+
+public class stringBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(" ");
+        for(char ch ='a'; ch<='z'; ch++){
+            sb.append(ch); //abcdefghijklmnopqrstuvwxyz
+        }
+        System.out.println(sb);
+    }
+}
