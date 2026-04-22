b567c5a91d1b9b2d863ca1c6b193da1d39a1c18c
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-04-22 18:58:14 +0530

    Print Sum of First N Natural Number Using Recursion

---
+++ src/firstNNatural.java
@@ -0,0 +1,16 @@
+package src;
+
+public class firstNNatural {
+    public static int calcsum(int n){
+        if(n==1){
+            return 1;
+        }
+        int snm1 = calcsum(n-1);
+        int sn = n+snm1;
+        return sn;
+    }
+    public static void main(String[] args) {
+        int n =5;
+        System.out.println(calcsum(n));
+    }
+}
