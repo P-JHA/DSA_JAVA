3ace177700a18e75537392bae1cb1557ecc2b1b0
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-04-27 22:58:53 +0530

    Tilling Problem

---
+++ src/tillingProblem.java
@@ -0,0 +1,21 @@
+package src;
+
+public class tillingProblem {
+    public static int tillingProblem(int n){
+        //base class
+        if(n==0 || n==1){
+            return 1;
+        }
+        //kaam
+        //vertical choice 
+        int fnm1 = tillingProblem(n-1);
+        //horizontal choice 
+        int fnm2 = tillingProblem(n-2);
+
+        int totalWays = fnm1+fnm2;
+        return totalWays;
+    }
+    public static void main(String[] args) {
+        System.out.println(tillingProblem(5));
+    }
+}
