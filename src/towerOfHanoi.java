f7966250be69c735cc0da2b28508f6112f35d345
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-05-05 22:38:11 +0530

    Tower of Hanoi recursion implementation

---
+++ src/towerOfHanoi.java
@@ -0,0 +1,25 @@
+package src;
+
+public class towerOfHanoi {
+
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+            return;
+        }
+
+        // transfer top n-1 from src to helper using dest
+        towerOfHanoi(n - 1, src, dest, helper);
+
+        // transfer nth disk from src to dest
+        System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+
+        // transfer n-1 from helper to dest using src
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        towerOfHanoi(n, "A", "B", "C");
+    }
+}
