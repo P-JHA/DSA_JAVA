34ce7e36f239bbd58114f0b789312159e167f1a2
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-05-19 22:48:17 +0530

    Backtracking Of Array

---
+++ src/BacktrackingArrays.java
@@ -0,0 +1,26 @@
+package src;
+
+public class BacktrackingArrays {
+    public static void ChangeArr(int arr[], int i, int val){
+        //base case
+        if(i==arr.length){
+            printArr(arr);
+            return;
+        }
+        //recursion 
+        arr[i] = val;
+        ChangeArr(arr, i+1, val+1);  //fnx call step
+        arr[i] = arr[i] - 2;  // bcaktracking step 
+    }
+    public static void printArr(int arr[]){
+        for (int i=0; i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = new int [5];
+        ChangeArr(arr, 0, 1);
+        printArr(arr);
+    }
+}
