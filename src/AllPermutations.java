6a154fcfd88ebf3711e011eafe629d1952fef19d
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-05-21 18:36:16 +0530

    Print all Permutations Of a String

---
+++ src/AllPermutations.java
@@ -0,0 +1,21 @@
+package src;
+
+public class AllPermutations {
+    public static void findpermutation(String str, String ans){
+        //base case
+        if(str.length()==0){
+            System.out.println(ans);
+        }
+        //recurssion 
+        for(int i=0; i<str.length(); i++){
+            char curr = str.charAt(i);
+            //"abcde" => "ab"+"de" = "abde"
+            String NewStr = str.substring(0, i)+str.substring(i+1);
+            findpermutation(NewStr, ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findpermutation(str, " ");
+    }
+}
