c5930de0afc435dab0b1a8b050f9709288a688c9
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-05-30 22:23:38 +0530

    Implement keypad combinations using BFS recursion

---
+++ src/keypadCombinations.java
@@ -0,0 +1,45 @@
+package src;
+
+public class keypadCombinations {
+
+    final static char[][] keypad = {
+            {}, {},
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'},
+            {'g', 'h', 'i'},
+            {'j', 'k', 'l'},
+            {'m', 'n', 'o'},
+            {'p', 'q', 'r', 's'},
+            {'t', 'u', 'v'},
+            {'w', 'x', 'y', 'z'}
+    };
+
+    public static void letterCombinations(String D) {
+        int len = D.length();
+
+        if (len == 0) {
+            System.out.println("");
+            return;
+        }
+
+        bfs(0, len, new StringBuilder(), D);
+    }
+
+    public static void bfs(int pos, int len, StringBuilder sb, String D) {
+
+        if (pos == len) {
+            System.out.println(sb.toString());
+            return;
+        }
+
+        char[] letters = keypad[Character.getNumericValue(D.charAt(pos))];
+
+        for (int i = 0; i < letters.length; i++) {
+            bfs(pos + 1, len, new StringBuilder(sb).append(letters[i]), D);
+        }
+    }
+
+    public static void main(String[] args) {
+        letterCombinations("2");
+    }
+}
